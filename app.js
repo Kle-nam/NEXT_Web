82f2f69119fc82e16d5feb834d733246e6547d16
Klenam Tsikata <99754468+Kle-nam@users.noreply.github.com>
2025-01-18 19:49:10 +0000

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-//const menu = document.querySelector('#mobile-menu')
-//const menu.links = document.querySelector('.navbar__menu')
-
-//menu.addEventListener('click', function(){
-   // menu.classList.toggle('is-active');
-   // menuLinks.classList.toggle('active');
-//});
 
 const navbarToggle = document.getElementById('mobile-menu');
 const navbarMenu = document.querySelector('.navbar__menu');
